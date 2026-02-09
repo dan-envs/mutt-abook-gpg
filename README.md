c0d9e9ea5a9504ae6008bacb963b9b74eaa59f27
~dan <dan@envs.net>
2026-02-09 20:25:06 +0100

    First publication

--- README.md
+++ README.md
@@ -1,3 +1,42 @@
 # abook-get-gpg-keys
+Receive new GPG keys from within Mutt or NeoMutt with the "abook" TUI program.
 
-Receive new GPG keys from within Mutt or NeoMutt
+**!!!THIS README.md IS A WORK-IN-PROGRESS!!!**
+
+# Setup
+1. Setup your '~/.gnupg/gpg.conf' as follows:
+
+```
+auto-key-locate wkd,keyserver
+keyserver hkps://keys.openpgp.org/
+auto-key-retrieve
+```
+
+2. Clone this repository to '~/.abook':
+`git clone https://git.envs.net/dan/abook-get-gpg-keys.git ~/.abook`
+
+## Setup for Mutt
+Add the following line to your '.muttrc':
+
+`source ~/.abook/abook.mutt.rc`
+
+### How to operate with Mutt
+**Start Mutt with the following command**: `mutt`
+**Add an email address to abook**: `'<SHIFT>+a'`
+**Receive public GPG/OpenPGP keys for all unknown contacts**: `'<CTRL>+a'`
+
+## Setup for NeoMutt
+Add the following line to your '~/.config/mutt/muttrc':
+
+`source ~/.abook/abook.neomutt.rc`
+
+Create an alias in your '.bashrc' or '.zsh' for `mutt`:
+
+`alias mutt="neomutt"`
+
+### How to operate with NeoMutt
+**Start Mutt with the following command**: `mutt` or `neomutt`
+**Add an email address to abook**: `'a'`
+**Receive public GPG/OpenPGP keys for all unknown contacts**: `'<SHIFT>+a'`
+**Query email contacts**:
+
---
+++ get_new_keys.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+readarray ALL_EMAILS
+CSV=$(mktemp --suffix=.csv)
+CONV_ABOOK=$(mktemp)
+
+KEYLIST="$(gpg --list-keys)"
+
+IFS=$'\n'
+for element in ${ALL_EMAILS[@]}; do
+  EMAIL=$(echo "${element}" | cut -f 1)
+  FOUND_EMAIL=$(printf '%s' "${KEYLIST}" | grep -F "${EMAIL}")
+  NAME=$(echo "${element}" | cut -f 2 | tr -d '\t' | sed -e 's/\s*$//')
+  if [[ -z "${FOUND_EMAIL}" ]]; then
+    if gpg --locate-keys --quiet "${EMAIL}" >/dev/null 2>&1;then
+      printf "ADDED: %s <%s>!\n" "${NAME}" "${EMAIL}"
+      printf "\"%s\",\"%s\",\"\",\"%s\",\"\"\n" "${NAME}" "${EMAIL}" "PGP yes" >>"${CSV}"
+    else
+      printf "NOT FOUND: %s <%s>\n" "${NAME}" "${EMAIL}"
+      printf "\"%s\",\"%s\",\"\",\"%s\",\"\"\n" "${NAME}" "${EMAIL}" "PGP no" >>"${CSV}"
+    fi
+  else
+    printf "Found: %s <%s>\n" "${NAME}" "${EMAIL}"
+      printf "\"%s\",\"%s\",\"\",\"%s\",\"\"\n" "${NAME}" "${EMAIL}" "PGP yes" >>"${CSV}"
+  fi
+done
+
+# create new addressbook
+cp "${HOME}"/.abook/addressbook "${HOME}/.abook/addressbook-$(date +%Y%m%d-%H%M).bak"
+abook --convert --informat csv --infile "${CSV}" --outformat abook --outfile "${CONV_ABOOK}"
+mv "${CONV_ABOOK}" "${HOME}"/.abook/addressbook
+rm "${CSV}"
+
+# Keep the last 8 backups
+rm "$(ls "${HOME}/.abook/addressbook-*" | head -n -8)"
